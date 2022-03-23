3ea68fa6e470f61f0350e5029236295544084444
Ana Xavier <78264318+ana-xavier@users.noreply.github.com>
2022-03-22 21:13:20 -0300

    Update ex3.java

--- 1 - 220318/ex3.java
+++ 1 - 220318/ex3.java
@@ -9,5 +9,8 @@ public class ex3
 {
     public static void main(String[] args)
     {
+        System.out.println(-5 + (8 * 6));
+        System.out.println((55+9) % 9);
+        System.out.println(20 + ( (-3*5) / 8 ));
     }
 }
