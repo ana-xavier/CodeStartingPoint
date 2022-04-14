ad77f1ef23562b781f5c1e10feeaa58b965d8ffc
Ana Xavier <78264318+ana-xavier@users.noreply.github.com>
2022-04-14 10:39:55 -0300

    Update ex04_1.java

--- 9 - 220413/ex04_1.java
+++ 9 - 220413/ex04_1.java
@@ -12,7 +12,7 @@ public class ex04_1
        int base = 2, potencia = 10, resultado = 1;
        
        //desenvolvimento
-       // ---> loop while irá continuar multiplicando a var de resultado pela var de base (2) até que a var resultado (10) se torne 0.
+       // ---> loop while irá continuar multiplicando a var de resultado pela var de base (2) até que a var potencia (10) se torne 0.
        while(potencia != 0){
            resultado = resultado * base ;
            System.out.println("---> " + resultado);
